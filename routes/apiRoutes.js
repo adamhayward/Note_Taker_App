3ebeb22db2ec4cafd9847dad9c11a984d87d4416
Adam Hayward <a.hayward11@gmail.com>
2020-12-19 20:01:55 -0900

    .gitignore updated

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,17 +1,24 @@
-var jsonF = require("../db/notes_db");
-var fs = require("fs");
-var path = require("path");
+const notes = require("../db/notes_db");
+const fs = require("fs");
+const path = require("path");
+
+const note = JSON.parse(fs.readFileSync(path.join(__dirname, "../db/notes_db.json")))
 
 module.exports = function (app) {
     //TODO GET /api/notes - Should read the db.json file and return all saved notes as JSON.
   app.get("/api/notes", function (req, res) {
-      var notes = JSON.parse(fs.readFileSync(path.join(__dirname, "../db/notes_db.json"))) 
-    return res.json(notes);
+    return res.json(note);
   });
   //TODO POST /api/notes - Should receive a new note to save on the request body, 
   //add it to the db.json file, and then return the new note to the client.
   app.post("/api/notes", function (req, res) {
-    res.json(notes);
+    var newNote = note.push(req.body);
+    return newNote;
+
+    var updateNote = JSON.stringify(note);
+    fs.writeFileSync("../db/notes_db.json", updateNote);
+    return res.json(req.body);
+
   });
   //TODO DELETE /api/notes/:id - Should receive a query parameter containing the id of a note to delete. This means you'll need to find a way to give each note a unique id when it's saved. In order to delete a note, you'll need to read all notes from the db.json file, 
   //remove the note with the given id property, and then rewrite the notes to the db.json file.
